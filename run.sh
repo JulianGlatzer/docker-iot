38474abf9ff1fbc44b47b0253dbaa7ebbf27efd3
Julian Glatzer <jglatzer@cern.ch>
2023-04-10 20:00:00 +0200

    updating ansible scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ansible-playbook ansible_iot.glatzer.eu.yml -i hosts
+if [ $# -eq 0 ]
+then
+    echo "Please provide lets encrypt email address"
+    exit 1
+fi
+
+ansible-playbook ansible_iot.glatzer.eu.yml -i hosts --extra-vars="email=$1" -v
